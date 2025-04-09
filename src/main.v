ecee99649ce6ced27d95a65643dc7970a439e0b9
Ross Hadden <rosshadden@gmail.com>
2025-04-08 23:15:49 -0400

    feat: marked required fields as such
    Except where it caused runtime errors...

--- src/main.v
+++ src/main.v
@@ -30,57 +30,70 @@ fn Generator.new(api_dump string) Generator {
 }
 
 struct GodotAPIClass {
-	name            string
-	is_refcounted   bool
-	is_instantiable bool
-	inherits        string
-	api_type        string
-	enums           []struct {
-		name        string
-		is_bitfield bool
+	name            string @[required]
+	is_refcounted   bool   @[required]
+	is_instantiable bool   @[required]
+	inherits        string // @[required]
+	api_type        string @[required]
+
+	enums []struct {
+		name        string          @[required]
+		is_bitfield bool            @[required]
 		values      []struct {
-			name  string
-			value f64
-		}
-	}
-	methods         []struct {
-		name               string
-		is_const           bool
-		is_vararg          bool
-		is_static          bool
-		is_virtual         bool
-		hash               f64
-		return_value       struct {
-			type string
+			name  string @[required]
+			value f64    @[required]
+		} @[required]
+	} // @[required]
+
+
+	methods []struct {
+		name       string @[required]
+		is_const   bool   @[required]
+		is_vararg  bool   @[required]
+		is_static  bool   @[required]
+		is_virtual bool   @[required]
+		hash       f64    @[required]
+
+		return_value struct {
+			type string @[required]
 			meta string
 		}
-		arguments          []struct {
-			name          string
-			type          string
+
+		arguments []struct {
+			name          string @[required]
+			type          string @[required]
 			default_value string
 			meta          string
 		}
+
 		hash_compatibility []f64
 		is_required        bool
-	}
-	properties      []struct {
-		type   string
-		name   string
+	} // @[required]
+
+
+	properties []struct {
+		type   string @[required]
+		name   string @[required]
 		setter string
-		getter string
+		getter string @[required]
 		index  f64
-	}
-	signals         []struct {
-		name      string
+	} // @[required]
+
+
+	signals []struct {
+		name      string @[required]
 		arguments []struct {
-			name string
-			type string
+			name string @[required]
+			type string @[required]
 		}
-	}
-	constants       []struct {
-		name  string
-		value f64
-	}
+	} // @[required]
+
+
+	constants []struct {
+		name  string @[required]
+		value f64    @[required]
+	} // @[required]
+
 }
 
 struct GodotApi {
