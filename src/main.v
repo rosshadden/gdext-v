3783048d3e5fcc08147f7af800272a437a5fc45b
Ross Hadden <rosshadden@gmail.com>
2025-04-08 23:40:39 -0400

    chore: ignored dump file

--- src/main.v
+++ src/main.v
@@ -2,7 +2,7 @@ import os
 
 fn main() {
 	println('\nGenerating V bindings...')
-	mut api_dump_file := 'extension_api.json'
+	mut api_dump_file := 'data/extension_api.json'
 
 	if os.args.len > 1 {
 		api_dump_file = os.args[1]
