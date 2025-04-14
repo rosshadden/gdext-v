ea2d7ba54a643b1f1029771d20eaf6c594f14667
Ross Hadden <rosshadden@gmail.com>
2025-04-14 11:43:15 -0400

    feat: this is no longer necessary

--- src/temp.v
+++
@@ -1,9 +0,0 @@
-module gd
-
-// TODO: this will be generated from the API dump in _GDExtension.v, once enums are mapped
-pub enum GDExtensionInitializationLevel as i64 {
-	initialization_level_core    = 0
-	initialization_level_servers = 1
-	initialization_level_scene   = 2
-	initialization_level_editor  = 3
-}
