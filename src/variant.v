3af5009118e143f2985565be10ee3f40ffc2969f
Ross Hadden <rosshadden@gmail.com>
2025-05-29 13:34:54 -0400

    feat: temp workaround for SceneTree#call_group

--- src/variant.v
+++ src/variant.v
@@ -165,3 +165,17 @@ pub fn (s &PackedScene) instantiate_as[T](cfg PackedScene_instantiate_Cfg) T {
 	scene := s.instantiate(cfg)
 	return scene.cast_to[T]()
 }
+
+// TEMP: workaround for SceneTree#call_group crashing
+pub fn (s &SceneTree) call_group_v(group string, method string, varargs ...Variant) {
+	nodes := s.get_nodes_in_group(group)
+	defer { nodes.deinit() }
+	for i in 0..nodes.size() {
+		var := nodes.get(i)
+		mut node := Node.new()
+		node.from_variant(var)
+		if node.has_method(method) {
+			node.call(method, ...varargs)
+		}
+	}
+}
