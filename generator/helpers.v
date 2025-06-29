1e62e19146936fd1965b1877b5c249692d04611d
Ross Hadden <rosshadden@gmail.com>
2025-06-29 13:38:24 -0400

    feat: better docs formatting, but also found some V issues that need fixed

--- generator/helpers.v
+++ generator/helpers.v
@@ -1,8 +1,16 @@
 module generator
 
-fn (g &Generator) build_docstring(comment string) string {
+// TODO: fix V stripping `#.*` from comments
+// TODO: fix V not working with the first item in a list
+
+fn (g &Generator) build_docstring(comments string) string {
 	mut lines := []string{}
-	for line in comment.split_into_lines() {
+	for comment in comments.split_into_lines() {
+		line := comment
+			.replace('[codeblock]', '```gdscript')
+			.replace('[/codeblock]', '```')
+			.replace('[code]', '`')
+			.replace('[/code]', '`')
 		lines << '// ${line}'
 	}
 	return lines.join('\n')
